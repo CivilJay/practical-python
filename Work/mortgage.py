b8e493e0f1b1d66515ddc99ffe1d9da31f3252c7
SL_Jacob <jakebejarano@gmail.com>
2023-10-26 13:54:47 -0700

    Finished Excs 1.7 - 1.12

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -9,22 +9,36 @@ total_paid = 0.0
 ExtraPaymentStartMonth = 61
 ExtraPaymentEndMonth = 108
 ExtraPayment = 1000
-
 month = 0
+
 while principal > 0:
+    principal = (principal * (1+rate/12)) - (payment)
+    total_paid = total_paid + payment
+    month = month + 1
+
+    # Test for extra payment and applies the payment 
     if ExtraPayment > 0:
-        if (month >= ExtraPaymentStartMonth & month <= ExtraPaymentEndMonth):
-            principal = principal * (1+rate/12) - (payment+ExtraPayment)
-            total_paid = total_paid + payment
-            month = month + 1
-            print(month,total_paid)
-        else: 
+        while (month >= ExtraPaymentStartMonth and month <= ExtraPaymentEndMonth):
+           # print ('zone', end='')  for debugging 
+            principal = principal - ExtraPayment
+            total_paid = total_paid + ExtraPayment
+            break
+        else:
             pass
-    principal = principal * (1+rate/12) - (payment)
-    total_paid = total_paid + payment
-    month = month + 1
-    print(month,total_paid)
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-    month = month + 1
-print('Total paid', total_paid,'in', month,'months')
+   
+   # code to correct for the over payment; the overpayment is negated
+    if principal < 0:
+        total_paid = total_paid + principal
+        principal = principal - principal
+
+    print(month,total_paid, principal,)
+    
+
+''' bool ('false') Evaluates to  True becuase a string is present 
+
+If the string were empty then converting it to a Bool as in:
+bool('') would evaluate to False
+'''
+
+
+"
